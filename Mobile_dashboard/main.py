4556ba8df703e0dd3844e8ee3de620c13df9f83f
Zibrov Dmitriy <zibrovdu@gmail.com>
2020-11-16 11:39:45 +0300

    Add new graph called site_stat (bar, with slider)

--- Mobile_dashboard/main.py
+++ Mobile_dashboard/main.py
@@ -1,13 +1,14 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
 from datetime import date
 
+
 current_month = date.today().month
-# Текущий фыормат файла с отчетом (горизонтальная таблица)
+# Текущий формат файла с отчетом (горизонтальная таблица)
 # df = pd.read_excel('data.xlsx', skiprows=7)
 # df = df.drop('Unnamed: 0', axis=1)
 # df.set_index(df.columns[0], inplace=True)
@@ -16,14 +17,19 @@ current_month = date.today().month
 # df['month'] = df['month'].dt.month
 
 # Предлагаемый формат файла с отчетом (вертикальная таблица)
-
 df = pd.read_excel('data.xlsx', sheet_name='Данные')
-# df = df.drop('Unnamed: 0', axis=1)
 df.set_index(df.columns[0], inplace=True)
-# df = df.T
 df['month'] = pd.to_datetime(df.index)
 df['month'] = df['month'].dt.month
-df.head()
+
+# Загружаем данные по посещению сайта
+site_df = pd.read_excel('data.xlsx', sheet_name='Данные по сайту', skiprows=5)
+site_df.drop(['Неделя 1', 'Unnamed: 6'], axis=1, inplace=True)
+site_df.rename(columns={'Unnamed: 0': 'Date'}, inplace=True)
+site_df.set_index(site_df.columns[0], inplace=True)
+site_df['month'] = pd.to_datetime(site_df.index)
+site_df['month'] = site_df['month'].dt.month
+
 
 external_stylesheets = ['assets/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -31,14 +37,14 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     html.Div([
         # html.H1("Межрегиональное бухгалтерское УФК"),
-        html.H2('Отчет о работе Отдела сопровождения пользователей МБУ ФК'),
+        html.H2('Отчет о работе Отдела сопровождения пользователей'),
         html.Img(src="assets/logo.png")
     ], className="banner"),
     html.Div([
         html.Div([
-            html.H3('Обращения в техническую поддержку'),
+            html.Center(html.H3('Обращения в тех поддержку')),
             dcc.Graph(id='users'),
-            dcc.Slider(id='month-slider_1',
+            dcc.Slider(id='month-slider_users',
                        min=df['month'].min(),
                        max=df['month'].max(),
                        value=current_month,
@@ -46,23 +52,37 @@ app.layout = html.Div([
                        step=None)
         ], className='six columns'),
         html.Div([
-            html.H3('Сопровождение пользователей'),
+            html.Center(html.H3('Сопровождение пользователей')),
             dcc.Graph(id='tech'),
-            dcc.Slider(id='month-slider_2',
+            dcc.Slider(id='month-slider_tech',
                        min=df['month'].min(),
                        max=df['month'].max(),
                        value=current_month,
                        marks={str(month): str(month) for month in df['month'].unique()},
                        step=None)
         ], className='six columns'),
-    ], className='row')
-])
+    ], className='row'),
+    html.Div([
+        html.Div([
+            html.H3(children="Статистика посещаний разделов сайта")
+        ], className='zagolovok'),
+        html.Div([
+            dcc.Graph(id='site'),
+            dcc.Slider(id='month-slider_site',
+                       min=df['month'].min(),
+                       max=df['month'].max(),
+                       value=current_month,
+                       marks={str(month): str(month) for month in df['month'].unique()},
+                       step=None)
+        ])
+    ], className='eleven columns')
+], className='row')
 
 
 @app.callback(
     Output('users', 'figure'),
-    [Input('month-slider_1', 'value')])
-def update_figure_user(selected_month_1):
+    [Input('month-slider_users', 'value')])
+def update_figure_user(selected_month_user):
     df1 = pd.read_excel('data.xlsx', skiprows=7)
     df1 = df1.drop('Unnamed: 0', axis=1)
     df1.set_index(df1.columns[0], inplace=True)
@@ -70,7 +90,7 @@ def update_figure_user(selected_month_1):
     df1['month'] = pd.to_datetime(df1.index)
     df1['month'] = df1['month'].dt.month
 
-    filtered_df_1 = df1[df1['month'] == selected_month_1]
+    filtered_df_1 = df1[df1['month'] == selected_month_user]
 
     trace_office = go.Scatter(x=list(filtered_df_1.index),
                               y=list(filtered_df_1['Сопровождение пользователя в офисе']),
@@ -95,7 +115,7 @@ def update_figure_user(selected_month_1):
                               name='удаленная работа',
                               visible=False)
 
-    data_1 = [trace_office, trace_online, trace_offline_bar, trace_online_bar]
+    data_user = [trace_office, trace_online, trace_offline_bar, trace_online_bar]
 
     updatemenus = list([
         dict(
@@ -121,7 +141,7 @@ def update_figure_user(selected_month_1):
         )
     ])
 
-    layout_1 = dict(
+    layout_user = dict(
         title='Обращения в техническую поддержку',
         updatemenus=updatemenus,
         autosize=False,
@@ -150,19 +170,19 @@ def update_figure_user(selected_month_1):
             rangeslider=dict(
                 visible=True
             ),
-            type='date_1'
+            type='date_user'
         )
     )
 
     return {
-        "data": data_1,
-        "layout": layout_1}
+        "data": data_user,
+        "layout": layout_user}
 
 
 @app.callback(
     Output('tech', 'figure'),
-    [Input('month-slider_2', 'value')])
-def update_figure_tech(selected_month_2):
+    [Input('month-slider_tech', 'value')])
+def update_figure_tech(selected_month_tech):
     df2 = pd.read_excel('data.xlsx', skiprows=7)
     df2 = df2.drop('Unnamed: 0', axis=1)
     df2.set_index(df2.columns[0], inplace=True)
@@ -170,7 +190,7 @@ def update_figure_tech(selected_month_2):
     df2['month'] = pd.to_datetime(df2.index)
     df2['month'] = df2['month'].dt.month
 
-    filtered_df_2 = df2[df2['month'] == selected_month_2]
+    filtered_df_2 = df2[df2['month'] == selected_month_tech]
 
     trace_rtk = go.Scatter(x=list(filtered_df_2.index),
                            y=list(filtered_df_2['РТК']),
@@ -207,7 +227,7 @@ def update_figure_tech(selected_month_2):
                                marker_color='blue',
                                visible=False)
 
-    data_2 = [trace_rtk, trace_sue, trace_sue_osp, trace_rtk_bar, trace_sue_bar, trace_sue_osp_bar]
+    data_tech = [trace_rtk, trace_sue, trace_sue_osp, trace_rtk_bar, trace_sue_bar, trace_sue_osp_bar]
 
     updatemenus = list([
         dict(
@@ -233,7 +253,7 @@ def update_figure_tech(selected_month_2):
         )
     ])
 
-    layout_2 = dict(
+    layout_tech = dict(
         title='Сопроводение пользователей',
         updatemenus=updatemenus,
         autosize=False,
@@ -262,13 +282,90 @@ def update_figure_tech(selected_month_2):
             rangeslider=dict(
                 visible=True
             ),
-            type='date_2'
+            type='date_user'
         )
     )
 
     return {
-        "data": data_2,
-        "layout": layout_2
+        "data": data_tech,
+        "layout": layout_tech
+    }
+
+
+@app.callback(
+    Output('site', 'figure'),
+    [Input('month-slider_site', 'value')])
+def update_figure_site(selected_month_site):
+    site_df_upd = pd.read_excel('data.xlsx', sheet_name='Данные по сайту', skiprows=5)
+    site_df_upd.drop(['Неделя 1', 'Unnamed: 6'], axis=1, inplace=True)
+    site_df_upd.rename(columns={'Unnamed: 0': 'Date'}, inplace=True)
+    site_df_upd.set_index(site_df_upd.columns[0], inplace=True)
+    site_df_upd['month'] = pd.to_datetime(site_df_upd.index)
+    site_df_upd['month'] = site_df_upd['month'].dt.month
+
+    filtered_site_df = site_df_upd[site_df_upd['month'] == selected_month_site]
+
+    trace_budget_bar = go.Bar(x=filtered_site_df.index,
+                              y=filtered_site_df['Электронный бюджет'],
+                              base=0,
+                              # marker_color='#F44242',
+                              name='Электронный бюджет')
+
+    trace_news_bar = go.Bar(x=filtered_site_df.index,
+                            y=filtered_site_df['Новости'],
+                            base=0,
+                            # marker_color='green',
+                            name='Новости')
+
+    trace_about_bar = go.Bar(x=filtered_site_df.index,
+                             y=filtered_site_df['О межрегиональном бухгалтерском УФК'],
+                             base=0,
+                             # marker_color='blue',
+                             name='О межрегиональном бухгалтерском УФК')
+
+    trace_other_bar = go.Bar(x=filtered_site_df.index,
+                             y=filtered_site_df['Иная деятельность'],
+                             base=0,
+                             # marker_color='blue',
+                             name='Иная деятельность')
+
+    data_site = [trace_budget_bar, trace_news_bar, trace_about_bar, trace_other_bar]
+
+    layout_site = dict(
+        title='Статистика посещаний разделов сайта',
+        autosize=False,
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=7,
+                         label='1w',
+                         step='day',
+                         stepmode='backward'),
+                    dict(count=14,
+                         label='2w',
+                         step='day',
+                         stepmode='backward'),
+                    dict(count=21,
+                         label='3w',
+                         step='day',
+                         stepmode='backward'),
+                    dict(count=28,
+                         label='4w',
+                         step='day',
+                         stepmode='backward'),
+                    dict(step='all')
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type='date_site'
+        )
+    )
+
+    return {
+        "data": data_site,
+        "layout": layout_site
     }
 
 
